78d1845a215e8addfc86d7294433e16905b7d65b
Vacayy <juyeong.kim.201@gmail.com>
2025-08-30 18:50:45 +0900

    feat: implement deploy script, change evm version to paris

--- 1a/script/MiniAMM.s.sol
+++ 1a/script/MiniAMM.s.sol
@@ -16,8 +16,11 @@ contract MiniAMMScript is Script {
         vm.startBroadcast();
 
         // Deploy mock ERC20 tokens
+        token0 = new MockERC20("Token X", "TKX");
+        token1 = new MockERC20("Token Y", "TKY");
 
         // Deploy MiniAMM with the tokens
+        miniAMM = new MiniAMM(address(token0), address(token1));
 
         vm.stopBroadcast();
     }
